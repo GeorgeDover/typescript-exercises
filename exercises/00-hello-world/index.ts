82897c3cbe6f9026e8ff2ac8e675de6c66229b5b
George Dover <65509207+GeorgeDover@users.noreply.github.com>
2026-05-03 19:47:58 -0700

    add exercises/00-hello-world

---
+++ exercises/00-hello-world/index.ts
@@ -0,0 +1,8 @@
+/* TODO: Add a type annotation to the `name` parameter 
+Hint: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#functions */
+
+export function greet(name) {
+  return `Hello, ${name}!`;
+}
+
+console.log(greet('world'));
