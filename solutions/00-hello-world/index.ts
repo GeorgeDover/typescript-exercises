80def751f6766f5e27ecfc3562c41a0dd9a4c9bb
George Dover <65509207+GeorgeDover@users.noreply.github.com>
2026-05-10 16:47:18 -0700

    add solutions/00-hello-world

--- solutions/00-hello-world/index.ts
+++ solutions/00-hello-world/index.ts
@@ -1,6 +1,6 @@
 /* TODO: Add a type annotation to the `name` parameter 
 Hint: https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#functions */
 
-export function greet(name) {
+export function greet(name: string) {
   return `Hello, ${name}!`;
 }
